e15f8011419189b3fb012d555d6fdaddd5d7cc72
jerr_alx <605231513@qq.com>
2018-11-11 22:51:29 +0800

    部门后台接口编写

--- src/main/java/com/mmall/controller/SysDeptController.java
+++ src/main/java/com/mmall/controller/SysDeptController.java
@@ -1,19 +1,26 @@
 package com.mmall.controller;
 
 import com.mmall.common.JsonData;
+import com.mmall.dto.DeptLevelDto;
 import com.mmall.param.DeptParam;
 import com.mmall.service.SysDeptService;
+import com.mmall.service.SysTreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @Controller
 @RequestMapping("/sys/dept")
 public class SysDeptController {
 
-    @Autowired
+    @Resource
     private SysDeptService sysDeptService;
+    @Resource
+    private SysTreeService sysTreeService;
 
     @RequestMapping("/save.json")
     @ResponseBody
@@ -21,6 +28,21 @@ public class SysDeptController {
         sysDeptService.save(param);
         return JsonData.success();
     }
+
+    @RequestMapping("tree.json")
+    @ResponseBody
+    public JsonData tree(){
+        List<DeptLevelDto> dtoList = sysTreeService.deptTree();
+        return JsonData.success(dtoList);
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public JsonData updateDept(DeptParam param) {
+        sysDeptService.update(param);
+        return JsonData.success();
+    }
+
 
 
 }
--- src/main/java/com/mmall/service/SysDeptService.java
+++ src/main/java/com/mmall/service/SysDeptService.java
@@ -1,15 +1,19 @@
 package com.mmall.service;
 
+import com.google.common.base.Preconditions;
 import com.mmall.dao.SysDeptMapper;
 import com.mmall.exception.ParamException;
 import com.mmall.model.SysDept;
 import com.mmall.param.DeptParam;
 import com.mmall.util.BeanValidator;
 import com.mmall.util.LevelUtil;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class SysDeptService {
@@ -45,5 +49,50 @@ public class SysDeptService {
         }
         return dept.getLevel();
     }
+
+    public void update(DeptParam param) {
+        BeanValidator.check(param);
+        if(checkExist(param.getParentId(), param.getName(), param.getId())) {
+            throw new ParamException("同一层级下存在相同名称的部门");
+        }
+
+        SysDept before = sysDeptMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(before, "待更新的部门不存在");
+        if(checkExist(param.getParentId(), param.getName(), param.getId())) {
+            throw new ParamException("同一层级下存在相同名称的部门");
+        }
+
+        SysDept after = SysDept.builder().id(param.getId()).name(param.getName()).parentId(param.getParentId())
+                .seq(param.getSeq()).remark(param.getRemark()).build();
+        after.setLevel(LevelUtil.calculateLevel(getLevel(param.getParentId()),param.getParentId()));
+        after.setOperator("Operator");
+        after.setOperateIp("OperateIp");
+        after.setOperateTime(new Date());
+
+        updateWithChild(before,after);
+
+    }
+
+    @Transactional
+    private void updateWithChild(SysDept before, SysDept after) {
+        String newLevelPrefix = after.getLevel();
+        String oldLevelPrefix = before.getLevel();
+
+        if(!after.getLevel().equals(before.getLevel())){
+            List<SysDept> deptList = sysDeptMapper.getChildDeptListByLevel(before.getLevel()); //该部门下的子部门
+            if(CollectionUtils.isNotEmpty(deptList)){
+                for(SysDept dept : deptList){
+                    String level = dept.getLevel();
+                    if(level.indexOf(oldLevelPrefix) == 0){ //说明了什么
+                        level = newLevelPrefix + level.substring(oldLevelPrefix.length());
+                        dept.setLevel(level);
+                    }
+                }
+                sysDeptMapper.batchUpdateLevel(deptList);
+            }
+        }
+        sysDeptMapper.updateByPrimaryKey(after);
+    }
+
 
 }
